2e21fafd43e7d7e92d4604ec65a8d3f35ad28ac0
Jezwin <edwin.eddu.8@gmail.com>
2019-10-09 11:13:45 +0530

    Introduced preventDefault() to avoid refreshing after AJAX

--- ui.apps/src/main/content/jcr_root/apps/auki/components/content/form/clientlib/js/form.js
+++ ui.apps/src/main/content/jcr_root/apps/auki/components/content/form/clientlib/js/form.js
@@ -1,12 +1,15 @@
 $(document).ready(function() {
-			$("#submit").click(function() {
+			$("#submit").click(function(event) {
+
+                event.preventDefault();
 				$.ajax({
 					type: "POST",
 					url: "/bin/test2",
                     data: $('form').serialize(),
+                    processData: false,
 					success: function(resp) {
-                        alert("Name: " + resp.name + "Description: " + resp.desc);
-                        
+                        alert("Name: " + resp.name + " Description: " + resp.desc);
+
             	}
 				});
 			});
